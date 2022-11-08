9a3e42d3ae776ee25e16d97be51f57d72bdcaa87
AprendizTI <aprendiz.ti@ivanagro.com>
2022-11-08 16:58:08 -0500

    modify in alert

--- app/Http/Controllers/CopyController.php
+++ app/Http/Controllers/CopyController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use DateTime;
 use DateTimeZone;
-use Alert;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class CopyController extends Controller
 {
